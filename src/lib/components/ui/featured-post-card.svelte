0e69148eef8736381fc05b9504cca06a905dc9b0
h <t3hwhistler2419@gmail.com>
2026-03-06 12:17:37 +0200

    feat(ui): improve featured post card layout

--- src/lib/components/ui/featured-post-card.svelte
+++ src/lib/components/ui/featured-post-card.svelte
@@ -11,6 +11,9 @@
 	import { Badge } from '$lib/components/ui/badge';
 	import PlusIcon from '@lucide/svelte/icons/plus';
 	import EditIcon from '@lucide/svelte/icons/edit';
+	import CalendarIcon from '@lucide/svelte/icons/calendar';
+	import ClockIcon from '@lucide/svelte/icons/clock';
+	import ChevronRightIcon from '@lucide/svelte/icons/chevron-right';
 	import XIcon from '@lucide/svelte/icons/x';
 
 	//const props = $props();
@@ -18,14 +21,15 @@
 </script>
 
 <Card class="overflow-hidden">
-	<div class="w-full p-4">
+	<div class="w-full p-6 pt-1 pb-0">
 		<img
-			src="/puneet.png"
+			src="https://picsum.photos/800"
 			alt="Featured"
 			class="aspect-video w-full rounded-xl border object-cover"
 		/>
 	</div>
 	<CardHeader>
+		<CardTitle class="text-4xl py-2">Featured Post Title</CardTitle>
 		<div class="flex flex-row gap-1">
 			<Badge variant="outline">Tag 1</Badge>
 			<Badge variant="outline">
@@ -41,15 +45,20 @@
 				<PlusIcon />
 			</Badge>
 		</div>
-		<CardTitle class="text-3xl">Featured Post Title</CardTitle>
-		<CardDescription>A very long featured post summary that describes the post.</CardDescription>
-		<CardDescription>by admin</CardDescription>
 	</CardHeader>
 	<CardContent>
 		<p class="text-muted-foreground">Lorem ipsum dolor sit amet, consectetur adipiscing elit...</p>
 	</CardContent>
-	<CardFooter class="gap-2">
-		<Button>Read Full Post</Button>
-		<Button class="icon" variant="outline"><EditIcon /></Button>
+	<CardFooter>
+		<div class="flex flex-row justify-between items-center w-full">
+			<div class="flex flex-col gap-1">
+				<p class="text-muted-foreground text-xs text-left">by admin</p>
+				<CardDescription class="flex items-center gap-1.5 text-xs text-left"><CalendarIcon class="size-4" />2026-01-01<ClockIcon class="size-4" />14:00</CardDescription>
+			</div>
+			<div class="flex gap-2">
+				<Button class="icon" variant="outline"><EditIcon /></Button>
+				<Button>Read Full Post<ChevronRightIcon /></Button>
+			</div>
+		</div>
 	</CardFooter>
 </Card>
