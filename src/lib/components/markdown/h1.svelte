be5135e300108201f0c57e22a5a8bcf96aa87b68
h <t3hwhistler2419@gmail.com>
2026-03-07 18:09:28 +0200

    feat(markdown): allow custom class on h1 component

--- src/lib/components/markdown/h1.svelte
+++ src/lib/components/markdown/h1.svelte
@@ -1,7 +1,10 @@
 <script>
-  let { children, ...props } = $props();
+	let { children, class: className = '', ...props } = $props();
 </script>
 
-<h1 {...props} class="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl mb-4">
-  {@render children?.()}
+<h1
+	{...props}
+	class={`mb-4 scroll-m-20 text-4xl font-bold tracking-tight lg:text-5xl ${className}`.trim()}
+>
+	{@render children?.()}
 </h1>
